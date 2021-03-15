6fb6fb0d305d2ba3e93ef2df4dc6173b5d451f7c
alo924 <76819874+alo924@users.noreply.github.com>
2021-03-15 18:38:13 -0500

    Update firebase.js
    Updating for firebase

--- netlify/functions/firebase.js
+++ netlify/functions/firebase.js
@@ -1,7 +1,14 @@
 const firebase = require("firebase/app")
 require("firebase/firestore")
 
-const firebaseConfig = {} // replace
+const firebaseConfig = {
+ apiKey: "AIzaSyALFZNKiUyPupdqN8A8ikfsFd-o14bC9OQ",
+    authDomain: "kiei451---final-project.firebaseapp.com",
+    projectId: "kiei451---final-project",
+    storageBucket: "kiei451---final-project.appspot.com",
+    messagingSenderId: "25268006381",
+    appId: "1:25268006381:web:0a1a8d7147e2273c36cbe4"
+}
 
 if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig)
